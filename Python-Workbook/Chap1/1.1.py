b071cdcb5bfcfec2fcdd5fabb444116deecc2cce
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2024-05-05 01:27:01 -0500

    Create 1.1.py

---
+++ Python-Workbook/Chap1/1.1.py
@@ -0,0 +1,4 @@
+name = "Huynh Quang Nguyen Vo"
+mailing_address = "240 ATRC, Athletic Avenue\n Oklahoma State University, Stillwater 74075"
+print(f"Name: {name}")
+print(f"Address: {mailing_address}")
